6dd28d2c4db3fbb1dddfe36d025b151463cae6d1
Jamie de Kloet <jamie.dekloet@student.hu.nl>
2020-04-08 13:27:50 +0200

    Added Simple tests

--- src/test/java/domain/RockPaperScissorsTest.java
+++ src/test/java/domain/RockPaperScissorsTest.java
@@ -1,5 +1,6 @@
 package domain;
 
+import domain.rules.RockPaperScissors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,8 +8,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class RockPaperScissorsTest {
     @Test
-    @DisplayName("Remove this test when setup works")
-    public void temporaryTest() {
-        assertTrue(false, "If you see this, it works and you can remove this method!");
+    @DisplayName("Rock crushes scissors")
+    void rockCrushesScissors() {
+        Rules game = new RockPaperScissors();
+        Result result = game.decide(Move.ROCK, Move.SCISSORS);
+
+        assertEquals(Result.WIN, result);
+    }
+
+    @Test
+    @DisplayName("Scissors cuts paper")
+    void scissorsCutsPaper() {
+        Rules game = new RockPaperScissors();
+        Result result = game.decide(Move.SCISSORS, Move.PAPER);
+
+        assertEquals(Result.WIN, result);
+    }
+
+    @Test
+    @DisplayName("Paper covers rock")
+    void paperCoversRock() {
+        Rules game = new RockPaperScissors();
+        Result result = game.decide(Move.PAPER, Move.ROCK);
+
+        assertEquals(Result.WIN, result);
     }
 }
